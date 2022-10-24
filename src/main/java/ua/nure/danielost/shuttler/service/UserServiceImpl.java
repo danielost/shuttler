3cc21c3687dfff81f5ee3cb025364a08b3ec398c
Daniel <danya.ostap@gmail.com>
2022-10-24 22:27:32 +0300

    Added user entity and endpoints

--- src/main/java/ua/nure/danielost/shuttler/service/UserServiceImpl.java
+++ src/main/java/ua/nure/danielost/shuttler/service/UserServiceImpl.java
@@ -1,4 +1,44 @@
 package ua.nure.danielost.shuttler.service;
 
-public class UserServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ua.nure.danielost.shuttler.exception.EmailTakenException;
+import ua.nure.danielost.shuttler.exception.EmptyTableException;
+import ua.nure.danielost.shuttler.exception.NoSuchUserException;
+import ua.nure.danielost.shuttler.model.User;
+import ua.nure.danielost.shuttler.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<User> getAllUsers() throws EmptyTableException {
+        if (userRepository.findAll().isEmpty()) {
+            throw new EmptyTableException("No users in database");
+        }
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUserById(long id) throws NoSuchUserException {
+        if (!userRepository.findById(id).isPresent()) {
+            throw new NoSuchUserException("No users with " + id + " id in database");
+        }
+
+        return userRepository.findById(id).get();
+    }
+
+    @Override
+    public User saveUser(User user) throws EmailTakenException {
+        if (userRepository.findByEmail(user.getEmail()) != null) {
+            throw new EmailTakenException("Email is already taken");
+        }
+
+        return userRepository.save(user);
+    }
 }
