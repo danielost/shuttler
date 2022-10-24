c746ee71da045157c1d3a37dbe5ff896abf7fa97
Daniel <danya.ostap@gmail.com>
2022-10-24 12:06:16 +0300

    Spring Boot project setup

---
+++ src/main/java/ua/nure/danielost/shuttler/ShuttlerApplication.java
@@ -0,0 +1,13 @@
+package ua.nure.danielost.shuttler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShuttlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShuttlerApplication.class, args);
+	}
+
+}
