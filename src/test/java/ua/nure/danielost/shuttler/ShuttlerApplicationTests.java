f426a23347d2bb7b74bd23cdcdd0d18d27909d07
Daniel <danya.ostap@gmail.com>
2022-10-24 12:06:16 +0300

    Spring Boot project setup

---
+++ src/test/java/ua/nure/danielost/shuttler/ShuttlerApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.nure.danielost.shuttler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShuttlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
