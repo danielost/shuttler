7668a6d4362b4fd14cf9d29763fc418942f4ecf5
Daniel <danya.ostap@gmail.com>
2022-11-11 18:23:41 +0200

    Improved authentication exception handling by returning objects as a response

--- src/main/java/ua/nure/danielost/shuttler/controller/AuthenticationControllerV1.java
+++ src/main/java/ua/nure/danielost/shuttler/controller/AuthenticationControllerV1.java
@@ -1,19 +1,14 @@
 package ua.nure.danielost.shuttler.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ua.nure.danielost.shuttler.dto.AuthenticationRequestDto;
-import ua.nure.danielost.shuttler.exception.NoSuchUserException;
 import ua.nure.danielost.shuttler.model.User;
 import ua.nure.danielost.shuttler.security.jwt.JwtTokenProvider;
 import ua.nure.danielost.shuttler.service.UserService;
