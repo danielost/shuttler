5eb43fc8c3d6ba86037f2915984dacd3b65dfd3d
Daniel <danya.ostap@gmail.com>
2022-12-04 17:37:57 +0200

    Added deployment prerequisites

--- src/main/java/ua/nure/danielost/shuttler/ShuttlerApplication.java
+++ src/main/java/ua/nure/danielost/shuttler/ShuttlerApplication.java
@@ -17,31 +17,31 @@ public class ShuttlerApplication {
 		SpringApplication.run(ShuttlerApplication.class, args);
 	}
 
-	@Bean
-	public ServletWebServerFactory servletContainer() {
-		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-			@Override
-			protected void postProcessContext(Context context) {
-				SecurityConstraint securityConstraint = new SecurityConstraint();
-				securityConstraint.setUserConstraint("CONFIDENTIAL");
-				SecurityCollection collection = new SecurityCollection();
-				collection.addPattern("/*");
-				securityConstraint.addCollection(collection);
-				context.addConstraint(securityConstraint);
-			}
-		};
-
-		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
-
-		return tomcat;
-	}
-
-	private Connector httpToHttpsRedirectConnector() {
-		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-		connector.setScheme("http");
-		connector.setPort(8080);
-		connector.setSecure(false);
-		connector.setRedirectPort(8443);
-		return connector;
-	}
+//	@Bean
+//	public ServletWebServerFactory servletContainer() {
+//		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+//			@Override
+//			protected void postProcessContext(Context context) {
+//				SecurityConstraint securityConstraint = new SecurityConstraint();
+//				securityConstraint.setUserConstraint("CONFIDENTIAL");
+//				SecurityCollection collection = new SecurityCollection();
+//				collection.addPattern("/*");
+//				securityConstraint.addCollection(collection);
+//				context.addConstraint(securityConstraint);
+//			}
+//		};
+//
+//		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+//
+//		return tomcat;
+//	}
+//
+//	private Connector httpToHttpsRedirectConnector() {
+//		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+//		connector.setScheme("http");
+//		connector.setPort(8080);
+//		connector.setSecure(false);
+//		connector.setRedirectPort(8443);
+//		return connector;
+//	}
 }
